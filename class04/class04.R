a96bc7e2bad72c5dc379832dd236d003a4c22f28
Scott MacLeod <scottmacleod@Scotts-MBP.lan>
2024-02-28 17:50:12 -0800

    I am adding class 4!

---
+++ class04/class04.R
@@ -0,0 +1,9 @@
+# My first R Script!!!!!!! <3
+
+x <- 1:50
+plot(x, sin(x))
+plot(x, sin(x), typ="l")
+plot(x, sin(x), typ="l", col="green")
+plot(x, sin(x), typ="l", col="green", lwd='3')
+plot(x, sin(x), typ="l", col="green", lwd='3', xlab="Hi there!")
+plot(x, sin(x), typ="l", col="green", lwd='3', xlab="Hi there!", ylab="My name is Scott!")
